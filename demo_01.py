8c4374a1ff200c0e37bb3994face0d6ec5948426
soichi-10 <182340748+soichi-10@users.noreply.github.com>
2024-12-23 08:22:06 +0900

    あ

--- demo_01.py
+++ demo_01.py
@@ -4,7 +4,9 @@ from pygame.locals import Rect
 pygame.init()
 
 clock = pygame.time.Clock()
+# ウィンドウサイズ
 screen = pygame.display.set_mode([640, 480])
+# ウィンドウの名前
 pygame.display.set_caption("pygame demo - window title here")
 
 running = True
@@ -17,6 +19,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    #　ウィンドウの背景の色
     screen.fill((238, 238, 170))  # back ground color
 
     pygame.draw.circle(screen, (176, 176, 222), (320, 240), 120)
@@ -24,9 +27,12 @@ while running:
     pygame.draw.circle(screen, (222, 176, 222), (120, 120), 20)
     pygame.draw.rect(screen, (120, 120, 120), Rect(120, 120, 200, 120))
 
+    #動く点のon,offそれぞれの色
     color_on = (240, 120, 120)
     color_off = (120, 120, 120)
+    # 動く点の四角の横の数
     for x0 in range(5):
+        #　動く点の四角の縦の数
         for y0 in range(7):
             # pygame.draw.circle(screen, color_off, (24 + x0 * 16, 24 + y0 * 16), 8)
             pygame.draw.rect(screen, color_off, Rect(24 + x0 * 16, 24 + y0 * 16, 12, 12))
@@ -36,6 +42,9 @@ while running:
     x1 += 1
     if x1 > 4:
         x1 = 0
+        y1 += 1
+        if y1 > 6:
+            y1 = 0
 
     pygame.display.flip()  # update
     clock.tick(5)  # FPS, Frame Per Second
