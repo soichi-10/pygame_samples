99b08f168b695450ca2e5c89eed5d82cce7f142d
soichi-10 <182340748+soichi-10@users.noreply.github.com>
2025-02-16 10:11:33 +0900

    a

--- demo_03.py
+++ demo_03.py
@@ -23,13 +23,13 @@ pygame.display.set_caption("pygame 7-segment display simulation")
 screen.fill(DARK_GRAY)
 
 ## 上から３つ目
-display1 = Seven_seg(screen)
+display1 = LCD_font(screen)
 display1.init_col(BLOCK_SIZE=9, BLOCK_INTV=10, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
 display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
 
 ## 上から２つ目
 display2 = LCD_font(screen)
-display2.init_col(BLOCK_SIZE=14, BLOCK_INTV=16, COLOR_ON=YELLOW, COLOR_OFF=GRAY)
+display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=YELLOW, COLOR_OFF=GRAY)
 display2.init_row(X_ORG=2, Y_ORG=18, COL_INTV=6)
 
 ## 上から１つ目
@@ -54,25 +54,34 @@ while running:
         time_now = (dt_now.hour * 10000
                     + dt_now.minute * 100
                     + dt_now.second)
+        date_now = (dt_now.year * 10000
+                    + dt_now.month * 100
+                    + dt_now.day)
 
-#        display1.update_col(col=0, num=count // (16 ** 3))   # 4096の位
-#        display1.update_col(col=1, num=count // (16 ** 2))   # 256の位
-#        display1.update_col(col=2, num=count // 16)          # 16の位
-#        display1.update_col(col=3, num=count)                # 1の位
+        display1.update_col(col=0, code=int(str(dt_now.year)[0]) + 48)
+        display1.update_col(col=1, code=int(str(dt_now.year)[1]) + 48)
+        display1.update_col(col=2, code=int(str(dt_now.year)[2]) + 48)
+        display1.update_col(col=3, code=int(str(dt_now.year)[3]) + 48)
+        display1.update_col(col=4, code=(ord('-')))
+        display1.update_col(col=5, code=dt_now.month // 10 + 48)
+        display1.update_col(col=6, code=int(str(dt_now.month)[0]) + 48)
+        display1.update_col(col=7, code=(ord('-')))
+        display1.update_col(col=8, code=int(str(dt_now.day)[0]) + 48)
+        display1.update_col(col=9, code=dt_now.day % 10 + 48)
 
-        display2.update_col(col=0, code=dt_now.hour // 10)
-        display2.update_col(col=1, code=dt_now.hour % 10)
-        display2.update_col(col=2, code=10)
-        display2.update_col(col=3, code=dt_now.minute // 10)
-        display2.update_col(col=4, code=dt_now.minute % 10)
-        display2.update_col(col=5, code=10)
-        display2.update_col(col=6, code=dt_now.second // 10)
-        display2.update_col(col=7, code=dt_now.second % 10)
+        display2.update_col(col=0, code=dt_now.hour // 10 + 48)
+        display2.update_col(col=1, code=dt_now.hour % 10 + 48)
+        display2.update_col(col=2, code=10 + 48)
+        display2.update_col(col=3, code=dt_now.minute // 10 + 48)
+        display2.update_col(col=4, code=dt_now.minute % 10 + 48)
+        display2.update_col(col=5, code=10 + 48)
+        display2.update_col(col=6, code=dt_now.second // 10 + 48)
+        display2.update_col(col=7, code=dt_now.second % 10 + 48)
 
 
-#        display3.disp_num2(zfil=False, rjust=3, num=count, base=10)
+        display3.disp_num2(zfil=False, rjust=3, num=count, base=10)
 
-#        display3.disp_num2(zfil=True, rjust=6, num=time_now, base=10)
+        display3.disp_num2(zfil=True, rjust=6, num=time_now, base=10)
 
         pygame.display.flip()  # update_col
         clock.tick(20)  # FPS, Frame Per Second
